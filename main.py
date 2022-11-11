7af16cb22333510e384ccf9e225854442eb598de
Botprojects <110056159+Botprojects@users.noreply.github.com>
2022-11-11 15:17:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+import os
+from pytube import YouTube
+from telebot import TeleBot,telebot,types
+
+bot = telebot.TeleBot('5118039174:AAE6E83OIk_kSsZlnbTs-yEZ05QkrqNEMG8')
+
+
+@bot.message_handler(commands=['start'])
+def welcome_message(msg):
+    bot.send_message(msg.chat.id,'Send valid youtube link')
+    
+    
+@bot.message_handler(func = lambda m:True)
+def audio_download(msg):
+    yt = YouTube(msg.text)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download('/storage/emulated/0/A')
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print('Downloading...')
+    with open(new_file,'rb') as e:
+        bot.send_audio(msg.chat.id,e,msg.message_id,caption=f'{yt.title}')
+
+bot.infinity_polling()
