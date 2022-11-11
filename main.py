05640b693d3241c045c62df5786dcfa91671f32b
Botprojects <110056159+Botprojects@users.noreply.github.com>
2022-11-11 19:40:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def welcome_message(msg):
 def audio_download(msg):
     yt = YouTube(msg.text)
     video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(or '.')
+    out_file = video.download('.')
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
