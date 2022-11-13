998289c0c44536173742c01f0328f3c941b41d46
Botprojects <110056159+Botprojects@users.noreply.github.com>
2022-11-13 11:57:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 
+
 import os
-from pytube import YouTube
+import pafy
 from telebot import TeleBot,telebot,types
+from telebot.util import user_link
 
 bot = telebot.TeleBot("5118039174:AAEbw77oG5lXpZJo1Wyl-mbXSiy8iB0Zzlc")
 
 
 @bot.message_handler(commands=['start'])
 def welcome_message(msg):
-    bot.send_message(msg.chat.id,'Send valid youtube link')
+    bot.send_message(msg.chat.id,f"Hello {user_link(msg.from_user)}Send valid youtube vidoe link")
     
     
 @bot.message_handler(func = lambda m:True)
 def audio_download(msg):
-    yt = YouTube(msg.text)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download()
-    base, ext = out_file
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print('Downloading...')
-    with open(new_file,'rb') as e:
-        bot.send_audio(msg.chat.id,e,msg.message_id)
+    downloader = pafy.new(msg.text)
+    audio = downloader.audiostreams[0]
+    fileName = f"{downloader.title}.mp3"
+    audio.download(fileName)
+    with open(file,"rb") as audio:
+        bot.send_audio(msg.chat.id,e,msg.message_id,caption="f{downloader.title}")
 
 bot.infinity_polling()
