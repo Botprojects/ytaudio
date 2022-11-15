0ccf0ca68c6db0b327bdc0d03539b778a29bc604
Botprojects <110056159+Botprojects@users.noreply.github.com>
2022-11-15 15:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,12 @@ def audio_download(msg):
         'keepvideo':False,
         'outtmpl':filename, }
         with youtube_dl.YoutubeDL(options) as ydl:
-            ydl.download([downloader['webpage_url']])
-            with open(filename,"rb")as audio:
+            file= ydl.download([downloader['webpage_url']])
+            bot.send_audio(msg.chat.id,file)
+            '''with open(filename,"rb")as audio:
                 bot.send_audio(msg.chat.id,audio,caption=f"{downloader['title']}")
                 bot.send_chat_action(msg.chat.id,action="upload_document")
-                os.remove(filename)
+                os.remove(filename)'''
     except Exception as e:
         print(e)
     
