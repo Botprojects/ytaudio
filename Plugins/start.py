14c29b0021b5477822c90960cd11ea190c756250
shamxl <shamxl@pm.me>
2023-12-01 13:51:08 +0530

    Set reply buttons

--- Plugins/start.py
+++ Plugins/start.py
@@ -1,9 +1,10 @@
 from pyrogram import Client, filters
-
+from Helpers.buttons import start_button
 
 @Client.on_message(filters.command("start") & filters.private)
 def handler (c, m):
     name = m.chat.first_name
     m.reply (
-        f"Hello {name}, welcome to youtube audio downloader bot\nSend a youtube video link to extract the audio of that video"
+        f"Hello {name}, welcome to youtube audio downloader bot\nSend a youtube video link to extract the audio of that video",
+        reply_markup = start_button
     )
