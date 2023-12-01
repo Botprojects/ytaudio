f4d4915e76dfd22df8e5b4d6441875d1833663dd
shamxl <shamxl@pm.me>
2023-12-01 13:50:46 +0530

    Add buttons

---
+++ Helpers/buttons.py
@@ -0,0 +1,14 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+start_button = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('🦧Official Channel🦧',url='t.me/neuralp')
+        ],
+        [
+            InlineKeyboardButton('🦅Official Group🦅',url='t.me/neuralg')
+        ]
+    ]
+)
+
