72332b4a7bb969a9e25b785e868655a078764b9a
shamxl <shamxl@pm.me>
2023-12-01 21:33:13 +0530

    Refactor async

--- Plugins/start.py
+++ Plugins/start.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from Helpers.buttons import start_button
 
 @Client.on_message(filters.command("start") & filters.private)
-def handler (c, m):
+async def handler (c, m):
     name = m.chat.first_name
-    m.reply (
+    await m.reply (
         f"Hello {name}, welcome to youtube audio downloader bot\nSend a youtube video link to extract the audio of that video",
         reply_markup = start_button
     )
